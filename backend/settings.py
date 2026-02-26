d1e2c9873322555f9fddb5dcd5dd7d7bc90bb787
Ashvita9 <Ashvita9@users.noreply.github.com>
2026-02-26 12:26:33 +0530

    feat: Initialize Django project settings with environment variables, REST Framework, CORS, Channels, and MongoDB integration.

--- backend/settings.py
+++ backend/settings.py
@@ -106,6 +106,36 @@ TEMPLATES = [
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://byte-bit-frontend.vercel.app",
+    "https://bytebit-backend.onrender.com",
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+APPEND_SLASH = True
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
